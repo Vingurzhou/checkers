93a0da9f4d9a7317db641579c4aa77f817e07ebd
vingurzhou <zwz0123460218@icloud.com>
2023-05-29 11:17:07 +0800

    feature(前端): 限制提交

---
+++ x/checkers/types/full_game.go
@@ -0,0 +1,47 @@
+/**
+ * Created by zhouwenzhe on 2023/5/29
+ */
+package types
+
+import (
+	"fmt"
+
+	sdkerrors "cosmossdk.io/errors"
+	"github.com/Vingurzhou/checkers/x/checkers/rules"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (storedGame StoredGame) GetBlackAddress() (black sdk.AccAddress, err error) {
+	black, errBlack := sdk.AccAddressFromBech32(storedGame.Black)
+	return black, sdkerrors.Wrapf(errBlack, ErrInvalidBlack.Error(), storedGame.Black)
+}
+
+func (storedGame StoredGame) GetRedAddress() (red sdk.AccAddress, err error) {
+	red, errRed := sdk.AccAddressFromBech32(storedGame.Red)
+	return red, sdkerrors.Wrapf(errRed, ErrInvalidRed.Error(), storedGame.Red)
+}
+
+func (storedGame StoredGame) ParseGame() (game *rules.Game, err error) {
+	board, errBoard := rules.Parse(storedGame.Board)
+	if errBoard != nil {
+		return nil, sdkerrors.Wrapf(errBoard, ErrGameNotParseable.Error())
+	}
+	board.Turn = rules.StringPieces[storedGame.Turn].Player
+	if board.Turn.Color == "" {
+		return nil, sdkerrors.Wrapf(fmt.Errorf("turn: %s", storedGame.Turn), ErrGameNotParseable.Error())
+	}
+	return board, nil
+}
+
+func (storedGame StoredGame) Validate() (err error) {
+	_, err = storedGame.GetBlackAddress()
+	if err != nil {
+		return err
+	}
+	_, err = storedGame.GetRedAddress()
+	if err != nil {
+		return err
+	}
+	_, err = storedGame.ParseGame()
+	return err
+}
--- x/checkers/types/types.go
+++ x/checkers/types/types.go
@@ -1,1 +1,10 @@
 package types
+
+import sdkerrors "cosmossdk.io/errors"
+
+// x/checkers module sentinel errors
+var (
+	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1103, "black address is invalid: %s")
+	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
+	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
+)
