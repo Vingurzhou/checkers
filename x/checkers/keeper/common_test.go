a62fc22f1587fbc4f1afe7986e964eac20318faf
vingurzhou <zwz0123460218@icloud.com>
2023-05-29 11:49:07 +0800

    feature(交易): 创建游戏，但是无后续操作

---
+++ x/checkers/keeper/common_test.go
@@ -0,0 +1,13 @@
+/**
+ * Created by zhouwenzhe on 2023/5/29
+ */
+
+package keeper_test
+
+import "github.com/Vingurzhou/checkers/x/checkers/testutil"
+
+const (
+	alice = testutil.Alice
+	bob   = testutil.Bob
+	carol = testutil.Carol
+)
